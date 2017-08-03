fb0b00715c2ec1b4da320b5a1324ee2a01c3ad3c
sogawa55 <s.ogawa055@gmail.com>
2017-08-04 01:06:56 +0900

    add comments

--- j-history-Quiz/PastDataManager.swift
+++ j-history-Quiz/PastDataManager.swift
@@ -1,19 +1,14 @@
-//
-//  PastData.swift
-//  j-history-Quiz
-//
-//  Created by shohei on 2017/03/07.
-//  Copyright © 2017年 history. All rights reserved.
-//
+
 
 import Foundation
 
 //AnswerViewControllerに表示する回答済みデータを格納する配列を宣言
 class PastDataManager{
+    //シングルトンオブジェクトを生成
     static let sharedInstance = PastDataManager()
-    
+    //問題文用の配列を宣言
     var PastDataArray = [String]()
-    
+    //正解番号用の配列を宣言
     var PastAnswerArray = [String]()
     
 }
