a03c84ce46732618771d1e4aa5da715354e7d24c
sogawa55 <s.ogawa055@gmail.com>
2017-08-04 01:44:32 +0900

    add comments

--- j-history-Quiz/RecordedData.swift
+++ j-history-Quiz/RecordedData.swift
@@ -5,14 +5,13 @@ import RealmSwift
 
 //過去の結果を保存するデータベース生成
 class RecordedData: Object{
-  
+    //選択した年代
     dynamic var RecordedEra: String = ""
-    
+    //正解率
     dynamic var RecordedScore: String = ""
-    
+    //回答時間
     dynamic var RecordedTime: String = ""
-    
+    //ランク
     dynamic var RecordedRank: String = ""
-   
     dynamic var created = Date()
 }
