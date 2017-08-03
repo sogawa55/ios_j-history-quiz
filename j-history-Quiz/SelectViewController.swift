bbd5f0ac76d88b79737095f8e5a1017156f57d22
sogawa55 <s.ogawa055@gmail.com>
2017-08-04 02:06:54 +0900

    add comments

--- j-history-Quiz/SelectViewController.swift
+++ j-history-Quiz/SelectViewController.swift
@@ -1,10 +1,4 @@
-//
-//  QuestionSelectViewController.swift
-//  j-history-Quiz
-//
-//  Created by shohei on 2017/03/05.
-//  Copyright © 2017年 history. All rights reserved.
-//
+
 
 import UIKit
 
@@ -13,18 +7,13 @@ class SelectViewController: UIViewController{
     override func viewDidLoad(){
         super.viewDidLoad()
         
-    
-    
   }
     
-    //時代を選択する
-
+    //回答する時代を選択する
     @IBAction func kodai(_ sender: Any) {
-        
-        
+        //シングルトンオブジェクトのプロパティに値を設定
         QuestionDataManager.sharedInstance.questionPath = "kodai"
-        
-        
+        //前回のデータの初期化
         PastDataManager.sharedInstance.PastDataArray.removeAll()
         
         EraData.sharedInstance.selectEra = "【原始・古代】"
@@ -44,8 +33,6 @@ class SelectViewController: UIViewController{
     
     @IBAction func kinsei(_ sender: Any) {
         
-
-        
         QuestionDataManager.sharedInstance.questionPath = "kinsei"
         
         PastDataManager.sharedInstance.PastDataArray.removeAll()
@@ -64,10 +51,7 @@ class SelectViewController: UIViewController{
         EraData.sharedInstance.selectEra = "【近代・現代】"
         
     }
-    
- 
-        
-    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -75,34 +59,32 @@ class SelectViewController: UIViewController{
         
     
     //問題データを初期化して回答画面に遷移
-    
     override func prepare(for segue: UIStoryboardSegue, sender:Any?){
-       
+        //シャッフルカウンターの初期化
         QuestionDataManager.sharedInstance.shuffleCounter = 0
+        //問題文の読み込み
         QuestionDataManager.sharedInstance.loadQuestion()
+        //問題番号の設定
         QuestionDataManager.sharedInstance.questionNo = 1
+        //問題キーの初期化
         QuestionDataManager.sharedInstance.nowQuestionIndex = 0
+        //問題をシャッフル
         QuestionDataManager.sharedInstance.shuffle()
 
-        
-        
+        //問題画面に遷移
         guard let nextViewController = segue.destination as?
             QuestionViewController else{
             return
         }
-        
+        //次の問題の読み込み
         guard let questionData = QuestionDataManager.sharedInstance.nextQuestion() else{
         return
         }
+        //遷移先の画面に読み込んだ問題データを渡す
         nextViewController.questionData = questionData
     }
     
- 
-    
-    @IBAction func goToSelectView(_ segue: UIStoryboardSegue){
-        
-
-       
+    @IBAction func goToSelectView(_ segue: UIStoryboardSegue){ 
     }
 }
 
